8e72d6598861f03c6b0b538abf35c0c2f5eed27d
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-03-09 18:57:58 +0300

    add main functional

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
+import { Todo } from "./todo";
+
 export class App {
-  message = "Hello World!";
+  constructor() {
+    this.message = "Hello World!";
+    this.todoList = [];
+    this.todoList.push(new Todo("Clean my room"));
+    this.todoList.push(new Todo("Walk the dog"));
+    this.todoList.push(new Todo("Take out the trash"));
+
+    this.newItem = "";
+  }
+
+  addTodo() {
+    this.todoList.push(new Todo(this.newItem));
+    this.newItem = "";
+  }
+
+  removeTodo(todo) {
+    this.todoList.splice(this.todoList.indexOf(todo), 1);
+  }
 }
